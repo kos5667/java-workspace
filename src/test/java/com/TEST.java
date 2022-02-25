aab3c8973a6e866eb7282360a7b599125940ac82
jh1025 <kos5667@gamil.com>
2022-02-25 13:39:44 +0900

    create repository

---
+++ src/test/java/com/TEST.java
@@ -0,0 +1,25 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+
+public class TEST {
+    
+    @Test
+    public void TEST1() throws Exception {
+
+        System.out.println("TEST 1");
+    }
+
+    @Test
+    public void TEST2() throws Exception {
+        
+        System.out.println("TEST 2");
+    }
+
+    @Test
+    public void TEST3() throws Exception {
+        
+        throw new Exception("TEST3");
+    }
+
+}
