f39f00afcf646aae000ed5450073e60862ec5c97
kos5667 <kos5667@gmail.com>
2022-03-13 03:45:45 +0900

    10818

--- src/test/java/com/algorithm/Q10818.java
+++ src/test/java/com/algorithm/Q10818.java
@@ -1,7 +1,25 @@
 package com.algorithm;
 
+import java.util.*;
 public class Q10818 {
     public static void main(String[] args) {
-        
+        try (Scanner scan = new Scanner(System.in)) {
+            int t = scan.nextInt();
+            List<Integer> list = new ArrayList<Integer>();
+
+            for(int i=0; i< t; i++){
+                int n = scan.nextInt();
+                list.add(n);   
+            }
+
+            int min = 1000000,max = -1000000;
+            for(int i : list) {
+                if(min > i)
+                    min = i;
+                if(max < i)
+                    max = i;
+            }
+            System.out.println(min + " " + max);
+        }
     }
 }
--- src/test/java/com/technology/ElasticSearchTest.java
+++ src/test/java/com/technology/ElasticSearchTest.java
@@ -1,18 +1,18 @@
 package com.technology;
 
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
-import org.elasticsearch.client.RestHighLevelClient;
+// import org.apache.http.HttpHost;
+// import org.elasticsearch.client.RestClient;
+// import org.elasticsearch.client.RestClientBuilder;
+// import org.elasticsearch.client.RestHighLevelClient;
 
-public class ElasticSearchTest {
+// public class ElasticSearchTest {
     
-    HttpHost host = new HttpHost("localhost", 9200, "http");
+//     HttpHost host = new HttpHost("localhost", 9200, "http");
 
-    RestClientBuilder restClientBuilder = RestClient.builder(host);
+//     RestClientBuilder restClientBuilder = RestClient.builder(host);
 
-    // RestHighLevelClient 지원 안함..
-    RestHighLevelClient client = new RestHighLevelClient(restClientBuilder);
+//     // RestHighLevelClient 지원 안함..
+//     RestHighLevelClient client = new RestHighLevelClient(restClientBuilder);
 
 
-}
+// }
