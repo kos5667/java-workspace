0f856e49fe64d32ed72ebe10e1abf006217b25cd
jh1025 <kos5667@gamil.com>
2022-03-18 16:10:12 +0900

    2581

---
+++ src/test/java/com/algorithm/BAEKJOON/silver/Q14888.java
@@ -0,0 +1,27 @@
+package com.algorithm.BAEKJOON.silver;
+import java.util.*;
+import java.io.*;
+/**
+ * 14888 연산자 끼워넣기
+ * 실버Ⅰ
+ */
+public class Q14888 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int o = n -1;
+
+        String[] nb = br.readLine().split(" ");
+        String[] ob = br.readLine().split(" ");
+        List<String> list = new ArrayList<String>();
+        for(int i=0; i<n+o; i++) {
+            if(i % 2 == 0) {
+            }
+        }
+    }
+
+    public static String[] getOperator() {
+        return null;
+    }
+}
--- src/test/java/com/algorithm/BAEKJOON/silver/Q1978.java
+++ src/test/java/com/algorithm/BAEKJOON/silver/Q1978.java
@@ -15,17 +15,16 @@ public class Q1978 {
                 int a = scan.nextInt();
                 int b = 0;
                 for(int j=1; j<=a; j++) {
-                    if(a%j == 0)
-                        b++;
+                    if(a%j == 0) b++;
                 }
-                if(b == 2)
-                    r++;
+                if(b == 2) r++;
             }
             System.out.println(r);
         }
     }
 
     public static boolean isPrime(int num) {
+        if(num == 1) return false;
         for(int i=2; i*i<num; i++) {
             if(num % i ==0) return false;
         }
