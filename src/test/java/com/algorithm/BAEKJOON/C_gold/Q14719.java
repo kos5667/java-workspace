114ebd1613d7cd0d7539fb3281c727fe2567eef4
kos5667 <kos5667@gmail.com>
2022-05-25 23:32:56 +0900

    image two int array

--- src/test/java/com/algorithm/BAEKJOON/C_gold/Q14719.java
+++ src/test/java/com/algorithm/BAEKJOON/C_gold/Q14719.java
@@ -6,6 +6,20 @@ package com.algorithm.BAEKJOON.C_gold;
  */
 public class Q14719 {
     public static void main(String[] args) {
-        
+        int x=5,y = 5;
+
+        int[][] box = new int[5][5];
+        for(int i=0; i<x; i++) {
+            for(int j=0; j<y; j++) {
+                box[i][j] = j;
+            }
+        }
+
+        for(int i=0; i<box.length; i++) {
+            for(int j : box[i]) {
+                System.out.print(i+","+j+" ");
+            }
+            System.out.println();
+        }
     }
 }
