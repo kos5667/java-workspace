7fea641849d423d356a1717cf37bad3efa0ef6aa
kos5667 <kos5667@gmail.com>
2022-05-29 23:34:42 +0900

    commit

--- src/test/java/com/algorithm/BAEKJOON/C_gold/Q14719.java
+++ src/test/java/com/algorithm/BAEKJOON/C_gold/Q14719.java
@@ -11,7 +11,7 @@ public class Q14719 {
     public static void main(String[] args) {
         try (Scanner scan = new Scanner(System.in)) {
             int h = scan.nextInt(), w = scan.nextInt();
-
+   
             int[] block = new int[w];
             for(int i=0; i<w; i++) {
                 block[i] = scan.nextInt();
@@ -24,14 +24,40 @@ public class Q14719 {
                 }
             }
 
-            for(int i=0; i<box.length; i++) {
-                for(int j : box[i]) {
-                    System.out.print("□ ");
-                    System.out.print("■ ");
-                }
-                System.out.println();
+            // run(block, box);
+            answer(block, box);
+        }
+    }
+
+    static void answer(int[] block, int[][] box) {
+        int start = 0, end = 0;
+        int sum = 0;
+        for(int i=0; i<block.length; i++) {
+            if(start <= block[i]) {
+                start = block[i];
+            } else {
+                System.out.println("S : "+start+" B :"+block[i]);
+                sum += start - block[i];
             }
         }
-        
+
+        System.out.println(sum);
+    }
+    
+    static void run(int[] block, int[][] box) {
+        for(int i=0; i<box.length; i++) {
+
+            // System.out.println("box[i].length : "+ box[i].length +" block[i] : " + block[i] + " M : "+ (box[i].length - block[i]));
+            for(int j=0; j<box[i].length; j++) {
+                int x = box.length;
+
+                if(((x-i) - block[j]) > 0) {
+                    System.out.print("□ ");
+                } else {
+                    System.out.print("■ ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
