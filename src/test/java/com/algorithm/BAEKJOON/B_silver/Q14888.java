19cd91add8478ba5395b06d3b392ffc8edc39d75
kos5667 <kos5667@gmail.com>
2022-06-30 23:10:56 +0900

    14888

--- src/test/java/com/algorithm/BAEKJOON/B_silver/Q14888.java
+++ src/test/java/com/algorithm/BAEKJOON/B_silver/Q14888.java
@@ -1,62 +1,67 @@
 package com.algorithm.BAEKJOON.B_silver;
 import java.util.*;
-import java.util.stream.IntStream;
 import java.io.*;
 /**
  * 14888 연산자 끼워넣기
  * 실버Ⅰ
  */
 public class Q14888 {
+
+    public static int MAX = Integer.MIN_VALUE;	// 최댓값 
+	public static int MIN = Integer.MAX_VALUE;	// 최솟값 
+	public static int[] operator = new int[4];	// 연산자 개수 
+	public static int[] number;					// 숫자 
+	public static int N;						// 숫자 개수 
+
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        int o = n -1;
-        
-        List<Integer> num_list = new ArrayList<Integer>();
-        IntStream.range(0, n).forEach(i  -> {
-            try {
-                int num = Integer.parseInt(br.readLine());
-                num_list.add(num);
-            } catch (NumberFormatException | IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        List<Integer> opt_list = new ArrayList<Integer>();
-        IntStream.range(0, 4).forEach(i  -> {
-            try {
-                int num = Integer.parseInt(br.readLine());
-                opt_list.add(num);
-            } catch (NumberFormatException | IOException e) {
-                e.printStackTrace();
-            }
-        });
-        
-        if(opt_list.stream().mapToInt(Integer::intValue).sum() > o) {
-            System.out.println("연산자는 n -1");
-        }
-
-        getSum(num_list, opt_list);
-
-        br.close();
-        bw.close();
+ 
+		N = Integer.parseInt(br.readLine());
+		number = new int[N];
+ 
+		// 숫자 입력 
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			number[i] = Integer.parseInt(st.nextToken());
+		}
+ 
+		// 연산자 입력 
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < 4; i++) {
+			operator[i] = Integer.parseInt(st.nextToken());
+		}
+ 
+		dfs(number[0], 1);
+ 
+		System.out.println(MAX);
+		System.out.println(MIN);
     }
 
-    public static String[] getSum(List<Integer> num_list, List<Integer> opt_list) {
-        String a = "";
-        for(int i=0; i < num_list.size() * opt_list.size(); i++) {
-        }
-        for(int i : num_list) {
-            a += Integer.toString(i);
-        }
-        
-        return null;
-    }
-    
-    public static String[] getOperator(List<Integer> num_list, List<Integer> opt_list) {
-
-
-        return null;
-    }
+    public static void dfs(int num, int idx) {
+		if (idx == N) {
+			MAX = Math.max(MAX, num);
+			MIN = Math.min(MIN, num);
+			return;
+		}
+ 
+		for (int i = 0; i < 4; i++) {
+			// 연산자 개수가 1개 이상인 경우
+			if (operator[i] > 0) {
+ 
+				// 해당 연산자를 1 감소시킨다.
+				operator[i]--;
+ 
+				switch (i) {
+ 
+				case 0:	dfs(num + number[idx], idx + 1);	break;
+				case 1:	dfs(num - number[idx], idx + 1);	break;
+				case 2:	dfs(num * number[idx], idx + 1);	break;
+				case 3:	dfs(num / number[idx], idx + 1);	break;
+ 
+				}
+				// 재귀호출이 종료되면 다시 해당 연산자 개수를 복구한다.
+				operator[i]++;
+			}
+		}
+	}
 }
