3bf801c64832d04cff34fb74591888ca24578905
kos5667 <kos5667@gmail.com>
2022-10-05 23:18:23 +0900

    Interface Function<T,R>

--- src/test/java/com/skill/java/Lambda/src/FunctionEx.java
+++ src/test/java/com/skill/java/Lambda/src/FunctionEx.java
@@ -5,13 +5,21 @@ import java.util.function.Function;
 public class FunctionEx {
     
     public static void main(String[] args) {
-
-        Function<Integer, String> function = (n) -> "parameter : " + n;
-        String result = function.apply(5);
-
-        System.out.println(result);
-
-        Function<Integer, Integer> function2 = (n) -> { return n * n; };
+        function1(2).apply(3); // 6
+        function2(2).apply(3); // 6
     }
 
+    public static Function<Integer, String> function1(int i) {
+        return (n) -> {return "result : " + (n * i);};
+    }
+
+    public static Function<Integer, String> function2(int i) {
+        return new Function<Integer, String>() {
+            @Override
+            public String apply(Integer n) {
+                // TODO Auto-generated method stub
+                return "result : " + (n * i);
+            }
+        };
+    }
 }
